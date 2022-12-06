3d0beb6c037e297a62ec548e26ef04b4afd9ce39
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-06 11:29:01 -0300

    feat: add create page view

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,5 +4,20 @@
         <div class="card-header">
                 Adicionar novo usuário
         </div>
+        <div class="card-body">
+            <form action="{{ url('user') }}" method='POST'>
+                    {!! csrf_field() !!}
+                    <div>
+                        <label for='name'>Name</label>
+                        <input type="text" name='name' id='name' class='form-control'>
+                    </div>
+                    <div>
+                        <label for='email'>Email</label>
+                        <input type="email" name='email' id='email' class='form-control'>
+                    </div>
+
+                    <input type="submit" value="Adicionar" class='btn btn-success'>
+                </form>
+        </div>
     </div>
-@endsection
+@stop
