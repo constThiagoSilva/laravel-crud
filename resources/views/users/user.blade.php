6933d49a6ab9eefa43a7183ec7ab76e9dcade61e
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-06 10:55:17 -0300

    feat: add users view page

---
+++ resources/views/users/user.blade.php
@@ -0,0 +1,14 @@
+@extends('user.layout')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Cadastro de Usuários</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
