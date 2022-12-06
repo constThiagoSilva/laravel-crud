fd6cc5ce65260a1377737091ff07a7232a8e1878
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-06 11:17:12 -0300

    feat: add crrate user route into add user button

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -7,6 +7,35 @@
                     <div class="card-header">
                         <h2>Cadastro de Usuários</h2>
                     </div>
+                    <div class="card-body">
+                        <a href="{{ url('/users/create') }}" class="btn btn-success btn-sm" title='add-user'>Adicionar usuário</a>
+                    </div>
+                    <br>
+                    <br>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>name</th>
+                                        <th>email</th>
+                                        <th></th>
+                                    </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Thiago</td>
+                                <td>thi@gmail.com</td>
+                                <td>
+                                    <a href="" title='view-user'><button class="btn btn-info btn-sm">Ver</button></a>
+                                    <a href="" title='edit-user'><button class="btn btn-primary btn-sm">Editar</button></a>
+                                    <a href="" title='delete-user'><button class="btn-danger btn-sm">Excluir</button></a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
